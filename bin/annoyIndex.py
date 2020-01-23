37586c1edb2726afea2fbfc9445ed3ce45912984
Gustavo Glusman <Gustavo@SystemsBiology.org>
2020-01-23 14:55:09 -0800

    Taught annoyIndex.py to normalize, changed kest.pl accordingly.

--- bin/annoyIndex.py
+++ bin/annoyIndex.py
@@ -2,11 +2,13 @@
 import sys, argparse
 from annoy import AnnoyIndex
 import re
+import statistics
 
 parser=argparse.ArgumentParser()
 parser.add_argument('--file', help='Input file')
 parser.add_argument('--out', help='Outfile base')
 parser.add_argument('--L', help='Fingerprint length')
+parser.add_argument('--norm', help='Normalize')
 args=parser.parse_args()
 
 a = AnnoyIndex(int(args.L))
@@ -18,8 +20,14 @@ with open(args.file, 'r') as f:
 		id, statements, *v = line.split("\t")
 		id = re.sub('.json.gz', '', id)
 		id = re.sub('\.', '|', id)
-		a.add_item(i, [float(j) for j in v])
 		names.append(id)
+		### normalize values if needed
+		v = [float(j) for j in v]
+		if args.norm:
+		    avg = statistics.mean(v)
+		    std = statistics.stdev(v)
+		    v = [(j-avg)/std for j in v]
+		a.add_item(i, v)
 		i = i+1
 
 a.build(-1)
