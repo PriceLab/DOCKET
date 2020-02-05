c8079fca923267088aeac7b4d794d102556c153f
Ryan <ryan@disco.systemsbiology.net>
2020-02-05 12:50:20 -0800

    Add Nextflow hello_docket example
    Add three new Python scripts to ingest a file, transform the ingested
    data to histograms (value occurrence counts), and calculate fingerprints
    on the histograms (for rows and columns in the original data). Add an
    encode_fp function in transform.py for encoding dictionary-of-dictionary
    data as fingerprints. Update hello_docket.py to make use of the new
    encode_fp function.
    
    Add a Nextflow hello_docket script that calls these new functions in a
    pipeline. Add a nextflow.config file and add $baseDir to the PYTHONPATH
    environment variable so that Python modules can be found during Nextflow
    script execution.

--- scripts/hello_docket.py
+++ scripts/hello_docket.py
@@ -1,12 +1,10 @@
-import sys
 import argparse
 import numpy as np
 from sklearn.cluster import AgglomerativeClustering
 
-sys.path.append('.')
-sys.path.append('..')
 import docket
-from docket.plugins.fingerprint import json2fp
+from docket import utilities
+from docket import transform
 
 parser = argparse.ArgumentParser()
 
@@ -26,29 +24,18 @@ parser.add_argument('--has_header', help='True if there are column labels', defa
 
 args = parser.parse_args()
 
-
-# skip_rows can be an int (indicating number of skipped lines) or a
-# str (specifying a comment character for rows to skip)
-def is_integer(value):
-    try:
-        int(value)
-        return True
-    except ValueError:
-        return False
-
-
 docket_params = {
     'files': args.files,
-    'skip_rows': int(args.skip_rows) if is_integer(args.skip_rows) else args.skip_rows,
+    'skip_rows': int(args.skip_rows) if utilities.is_integer(args.skip_rows) else args.skip_rows,
     'skip_cols': int(args.skip_cols),
     'has_header': str(args.has_header).lower() in ('true', '1'),
     'has_index': str(args.has_index).lower() in ('true', '1')
 }
 
-dm1 = docket.DocketMaker(**docket_params)
-data = dm1.file_data
+dm = docket.DocketMaker(**docket_params)
+data = dm.file_data
 
-for label, mdata in dm1.file_metadata.items():
+for label, mdata in dm.file_metadata.items():
     # Get contents of metadata
     file_type = mdata['file_type']
     file_path = mdata['file_path']
@@ -64,25 +51,15 @@ for label, mdata in dm1.file_metadata.items():
     # Convert tabular to json
     current_data = data[label]
     current_data = [[val.lower() for val in row] for row in current_data]
-    json_data = docket.transform.tabular2json(current_data, mdata['row_labels'], mdata['col_labels'])
+    json_data = transform.tabular2json(current_data, mdata['row_labels'], mdata['col_labels'])
 
     # Get original data (for later use)
     level1_headers = list(json_data.keys())
     level2_headers = list(json_data[level1_headers[0]].keys())
     original_data = [[v for k, v in json_data[header].items()] for header in level1_headers]
 
-    # Convenience function for generating fingerprints
-    def encode_fp(encoder, data_sets):
-        encodings = {}
-        for data_label, data_ in data_sets.items():
-            encoder.recurse_structure(data_)
-            encodings[data_label] = encoder.fp
-            encoder.reset()
-        return encodings
-
     # Calculate fingerprints
-    fp1 = json2fp.DataFingerprint(**{'length': int(args.L)})
-    fingerprints = encode_fp(fp1, json_data)
+    fingerprints = transform.encode_fp(json_data, args.L)
 
     # Convert to numpy array for analysis
     fp_array = np.array([data for _, data in fingerprints.items()])
