a74465b999404399eefc898edcc05550610d8fe0
Gustavo Glusman <Gustavo@SystemsBiology.org>
2020-02-06 11:49:11 -0800

    Moved stuff from bin to scripts.

---
+++ scripts/topMatch.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/env perl
+$|=1;
+use strict;
+
+my(%qseen, %tseen);
+while (<>) {
+	chomp;
+	my($q, $t, $c) = split /\t/;
+	next if $qseen{$q};
+	next if $tseen{$t};
+	print "$_\n";
+	$qseen{$q}++;
+	$tseen{$t}++;
+}
