0424bcc2382eebaeaea70773826312fe12a46dbb
Gustavo Glusman <Gustavo@SystemsBiology.org>
2020-02-14 21:31:05 -0800

    Implemented column type detection in scripts/ingest_pd.py

--- common/transform.py
+++ common/transform.py
@@ -10,7 +10,7 @@
 # -----------------------------------------------------------------------------
 import numpy as np
 import common.utilities as utilities
-from datafingerprint import DataFingerprint
+#from datafingerprint import DataFingerprint
 
 
 # -------------------------------------------------------------------------
