6a3248ba4e191ebf85be31d4ee08a92fde9478f8
Gustavo Glusman <Gustavo@SystemsBiology.org>
2020-02-17 10:55:17 -0800

    Determining column/row type in compute_hist.pl

--- scripts/compute_hist.pl
+++ scripts/compute_hist.pl
@@ -3,8 +3,7 @@ $|=1;
 use strict;
 use JSON;
 
-my($storeddata) = @ARGV;
-
+my($storeddata, $hist_outfile, $types_outfile, $decimals) = @ARGV;
 my $json;
 open DATA, "gunzip -c $storeddata |";
 while (<DATA>) {
@@ -13,27 +12,75 @@ while (<DATA>) {
 }
 close DATA;
 my $data = decode_json($json);
-my $ch = compute_content_histogram($data);
-print to_json($ch, {pretty=>1});
+my($ch, $types) = compute_content_histogram($data);
+foreach (keys %$ch) {
+	$types->{$_}{'values'} = scalar keys %{$ch->{$_}};
+}
+
+open TYPES, ">$types_outfile";
+print TYPES to_json($types, {pretty=>1});
+close TYPES;
+
+open HIST, ">$hist_outfile";
+print HIST to_json($ch, {pretty=>1});
+close HIST;
 
 
 sub compute_content_histogram {
 	my($what) = @_;
-	my %hist;
+	my(%hist, %types);
 
 	while (my($id, $ref) = each %$what) {
 		foreach my $value (values %$ref) {
-			next unless $value;
+			#next unless $value;
 			if (ref $value eq 'ARRAY') {
-				foreach (@{$value}) {
+				foreach my $v (@{$value}) {
 					## modify to get rid of surrounding quotes, trimming numerical resolution, other cleanups
-					$hist{$id}{$_}++ if $_;
+					$value = $1 if $value =~ /^\"(.+)\"$/;
+					if (isnumeric($v)) {
+						$types{$id}{'NUM'}++;
+						if ($decimals && $v =~ /\./) {
+							$value = sprintf("%.${decimals}f", $v);
+						}
+					} elsif ($v eq '') {
+						$types{$id}{'NULL'}++;
+					} else {
+						$types{$id}{'STR'}++;
+					}
+					
+					$hist{$id}{$v}++ if $v;
 				}
 			} else {
 				## modify to get rid of surrounding quotes, trimming numerical resolution, other cleanups
+				$value = $1 if $value =~ /^\"(.+)\"$/;
+				if (isnumeric($value)) {
+					$types{$id}{'NUM'}++;
+					if ($decimals && $value =~ /\./) {
+						$value = sprintf("%.${decimals}f", $value);
+					}
+				} elsif ($value eq '') {
+					$types{$id}{'NULL'}++;
+				} else {
+					$types{$id}{'STR'}++;
+				}
+				
 				$hist{$id}{$value}++;
 			}
 		}
 	}
-	return \%hist;
+	return \%hist, \%types;
+}
+
+sub isnumeric ($) {
+        no warnings;
+        my $v = $_[0];
+        $v =~ s/0+$// if $v =~ /\.\d*?0+/;
+        $v =~ s/\.$//;
+        if (substr($v,0,1) eq '.') {
+                return "0$v" eq $v+0;
+        } elsif ($v =~ /^([\-\+])\.(.+)/) {
+                return "${1}0.$2" eq $v+0;
+        } else {
+                return $v eq $v+0;
+        }
 }
