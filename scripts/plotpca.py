7714dc3c520c92967e2cf6e092da6cb59dc81eef
Gustavo Glusman <Gustavo@SystemsBiology.org>
2020-02-19 10:51:59 -0800

    Initial implementation of coloring PCA by hierarchical clusters.

--- scripts/plotpca.py
+++ scripts/plotpca.py
@@ -7,16 +7,20 @@ import numpy as np
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+import gzip
 
 parser=argparse.ArgumentParser()
 parser.add_argument('infile', nargs='?', default=sys.stdin, help='File with PCA values')
 parser.add_argument('outfile', nargs='?', default=sys.stdin, help='Where to save the plot')
+parser.add_argument('--clustering', help='File with clustering information')
+parser.add_argument('--clusters', help='Number of clusters to display', default=3)
 parser.add_argument('--components', help='Number of components', default=2)
 parser.add_argument('--c1', help='First component (x axis)', default=0)
 parser.add_argument('--c2', help='Second component (y axis)', default=1)
 args=parser.parse_args()
 infile = args.infile
 outfile = args.outfile
+clustersfile = args.clustering
 c = int(args.components)
 c1 = int(args.c1)
 c2 = int(args.c2)
@@ -25,10 +29,21 @@ c2 = int(args.c2)
 names = np.genfromtxt(fname = infile, dtype='str', usecols = range(0, 1))
 Y = np.genfromtxt(fname = infile, dtype='float', usecols = range(1, c+1))
 
+color = 'black'
+# read clustering information if available
+if clustersfile:
+    with gzip.open(clustersfile, 'rb') as cf:
+        ids = cf.readline()
+        throwaway = cf.readline()
+        clust = cf.readline().decode('utf8').strip()
+        clust = clust.split('\t')
+        color = clust[1:]
+        color = [float(c)/2 for c in color]
+
 # plot
 tp = np.array(Y).transpose()
 plt.figure()
 if len(names)>3:
-    plt.scatter(tp[c1],tp[c2],color='black',marker='.',s=1)
+    plt.scatter(tp[c1],tp[c2],c=color,marker='o',s=1)
 
 plt.savefig(outfile)
