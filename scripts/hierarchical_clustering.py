e288682206fdf38a6aaf62f74bda6e197d1acd77
Ryan <ryan@disco.systemsbiology.net>
2020-02-25 11:01:34 -0800

    Incorporate enrichment into docket_study pipeline
    Incorporate new enrichment processes from test_docket_study.nf into the
    main docket_study (Nextflow script) pipeline. As part of this merge,
    update hierarchical_clustering.py to write out (to file) the linkage
    table (from hierarchical clustering), which is needed by the downstream
    enrichment-related processes.

--- scripts/hierarchical_clustering.py
+++ scripts/hierarchical_clustering.py
@@ -11,6 +11,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--source', help='File to load')
 parser.add_argument('--method', default='ward', help='Linkage method')
 parser.add_argument('--criterion', default='distance', help='The criterion to use informing clusters')
+parser.add_argument('--linkage_out', default='hier_cluster_linkage.txt.gz', help='Clustering linkage table output file')
 args = parser.parse_args()
 
 # Load data to be clustered
@@ -20,18 +21,19 @@ data = pd.read_table(args.source, index_col=0, header=None)
 labels = data.index
 
 # Get data as numpy array and normalize
-values = normalize(data.values)
+values = data.values
+#values = normalize(values)
 
-# Perform clustering
-linkage = shc.linkage(values, method=args.method)
+# Perform clustering and write linkage table to output file
+linkage_table = shc.linkage(values, method=args.method)
+pd.DataFrame(linkage_table).to_csv(args.linkage_out, sep='\t', index=False, header=False)
 
 # Generate a list of dendrogram cutoff values
-distances, num_distances = linkage[:, 2], len(linkage[:, 2])
+distances, num_distances = linkage_table[:, 2], len(linkage_table[:, 2])
 cutoff_values = [np.mean(distances[i: min(i+2, num_distances)]) for i in range(num_distances)]
-cutoff_values = np.unique(cutoff_values)
 
 # Generate cluster assignments at different cutoff values
-cluster_assignments = [fcluster(linkage, c, criterion=args.criterion) for c in reversed(cutoff_values)]
+cluster_assignments = [fcluster(linkage_table, c, criterion=args.criterion) for c in reversed(cutoff_values)]
 
 # Write results to stdout
 index = list(range(len(cluster_assignments)))
