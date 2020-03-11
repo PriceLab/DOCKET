6664ec11b5ee81c221543818611a31aec632419f
gloria <guangrong86@gmail.com>
2020-03-10 21:43:30 -0700

    revised integration for better paraterization

--- integration/run.sh
+++ integration/run.sh
@@ -1,25 +1,32 @@
 #!/bin/sh
 
+######### Usages #########
+
+input=$1
+output=$2
+
+
 echo "Comparison:-----------------------------"
-python docket_integration_interface.py Output/ comp \
+
+python docket_integration_interface.py comp $input $output \
     config/Para_sim.json
 
 echo "Integration-mut-drug response:----------"
-python docket_integration_interface.py Output/  mut_drugResponse \
+python docket_integration_interface.py mut_drugResponse $input $output \
     config/Para1_integration.json \
     config/Para2_integration.json
 
 echo "Integration-drug annotation:-------------"
-python docket_integration_interface.py Output/  annotation \
+python docket_integration_interface.py  annotation $input $output \
     config/Para1_annotation.json \
     config/Para2_annotation.json
 
 echo "Integration-visulization: ---------------"
-python docket_integration_interface.py Output/  visualization \
+python docket_integration_interface.py  visualization $input $output \
     config/Para_visualization.json
 
 echo "Integration-mut-expression: -------------"
-python docket_integration_interface.py Output/  mut_expr \
+python docket_integration_interface.py mut_expr $input $output\
     config/Para1_integration_mut_expr.json \
     config/Para2_integration_mut_expr.json
 
